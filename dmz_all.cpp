e67843b2dc813c7854a4bd66dab3224e642529e2
Dave Goldman <dgoldman@ebay.com>
2014-12-01 10:51:16 -0800

    update dmz_all.cpp

--- dmz_all.cpp
+++ dmz_all.cpp
@@ -6,7 +6,6 @@
 
 #if COMPILE_DMZ
 
-#include <sys/time.h>
 #include "./cv/canny.cpp"
 #include "./cv/conv.cpp"
 #include "./cv/convert.cpp"
@@ -19,13 +18,13 @@
 #include "./dmz.cpp"
 #include "./dmz_olm.cpp"
 #include "./geometry.cpp"
+#include "./models/expiry/modelc_ab0b6054.cpp"
+#include "./models/expiry/modelc_e1533ae9.cpp"
+#include "./models/expiry/modelm_730c4cbd.cpp"
 #include "./models/generated/modelc_01266c1b.cpp"
 #include "./models/generated/modelc_5c241121.cpp"
 #include "./models/generated/modelc_b00bf70c.cpp"
 #include "./models/generated/modelm_befe75da.cpp"
-#include "./models/expiry/modelm_730c4cbd.cpp"
-#include "./models/expiry/modelc_e1533ae9.cpp"
-#include "./models/expiry/modelc_ab0b6054.cpp"
 #include "./mz.cpp"
 #include "./mz_android.cpp"
 #include "./processor_support.cpp"
