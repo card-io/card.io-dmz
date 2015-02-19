26bcfa1dc233d85b66efd2be78037f61c8245cd5
Dave Goldman <dgoldman@ebay.com>
2015-02-18 16:46:59 -0800

    enable Eigen assertions etc, for Debug builds

--- eigen.h
+++ eigen.h
@@ -12,13 +12,15 @@
 
 #define EIGEN_NO_MALLOC 1 // disable all heap allocation of matrices
 
-// turn off range checking, asserts, and anything else that could slow us down
-// NB: turning off asserts is important! When EIGEN_SAFE_TO_USE_STANDARD_ASSERT_MACRO is 0,
-// as it is with llvm-gcc4.2 (which is our current iOS compiler), then a very slow
-// assertion macro replacement is used. As of time of writing, it cuts the 4S from
-// ~22fps to ~17fps.
-#define NDEBUG 1
-#define EIGEN_NO_DEBUG 1
+#if !DEBUG
+  // turn off range checking, asserts, and anything else that could slow us down
+  // NB: turning off asserts is important! When EIGEN_SAFE_TO_USE_STANDARD_ASSERT_MACRO is 0,
+  // as it is with llvm-gcc4.2 (which is our current iOS compiler), then a very slow
+  // assertion macro replacement is used. As of time of writing, it cuts the 4S from
+  // ~22fps to ~17fps.
+  #define NDEBUG 1
+  #define EIGEN_NO_DEBUG 1
+#endif
 
 #include "Eigen/Core"
 #include "Eigen/Dense"
