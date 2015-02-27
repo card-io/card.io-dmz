3f9a431c0d35c172f26773759e1ac63055edf806
Dave Goldman <dgoldman@ebay.com>
2015-02-26 17:45:37 -0800

    refactor stripe detection

--- scan/stripes.cpp
+++ scan/stripes.cpp
@@ -16,48 +16,30 @@
 //
 // Maximum pixel value possible with abs(Scharr) operator = 3 * 255 + 10 * 255 + 3 * 255 = 4080, so use a 16S destination.
 // Maximum line-sum possible = 4080 * kCreditCardTargetWidth = 1,746,240 = 0x1AA540, so line-sum will always fit in a long.
-// Maximum stripe-sum possible = (max line-sum possible) * kSmallCharacterHeight = 26,193,600 = 0x18FAEC0, so stripe-sum will always fit in a long.
+// Maximum stripe-sum possible = (max line-sum possible) * kNumberHeight = 47,148,480 = 0x2CF6DC0, so stripe-sum will always fit in a long.
 
 struct StripeSumCompareDescending
 : public std::binary_function<StripeSum, StripeSum, bool> {
   inline bool operator()(StripeSum const &stripe_sum_1, StripeSum const &stripe_sum_2) const {
+    if (stripe_sum_1.height != stripe_sum_2.height) {
+      return (stripe_sum_1.height > stripe_sum_2.height);
+    }
     return (stripe_sum_1.sum > stripe_sum_2.sum);
   }
 };
 
-std::vector<StripeSum> sorted_stripes(IplImage *card_y) {
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
+std::vector<StripeSum> sorted_stripes(IplImage *sobel_image, uint16_t starting_y_offset, int minCharacterHeight, int maxCharacterHeight) {
+#if DEBUG_STRIPES_PERFORMANCE
   dmz_debug_timer_start();
 #endif
   
-  CvSize card_image_size = cvGetSize(card_y);
-  
-  // Look for vertical line segments -> sobel_image:
-  
-  IplImage *sobel_image = cvCreateImage(card_image_size, IPL_DEPTH_16S, 1);
-  cvSetZero(sobel_image);
-  
-  CvRect below_numbers_rect = cvRect(0, starting_y_offset + kNumberHeight, card_image_size.width, card_image_size.height - (starting_y_offset + kNumberHeight));
-  cvSetImageROI(card_y, below_numbers_rect);
-  cvSetImageROI(sobel_image, below_numbers_rect);
-  
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
-  dmz_debug_timer_print("set up for Sobel");
-#endif
-  
-  llcv_scharr3_dx_abs(card_y, sobel_image);
-  
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
-  dmz_debug_timer_print("do Sobel [Scharr]");
-#endif
-  
-  cvResetImageROI(card_y);
-  cvResetImageROI(sobel_image);
+  CvSize card_image_size = cvGetSize(sobel_image);
+  CvRect relevant_rect = cvRect(0, starting_y_offset, card_image_size.width, card_image_size.height - starting_y_offset);
   
   // Calculate relative vertical-line-segment-ness for each scan line (i.e., cvSum of the [x-axis] Sobel image for that line):
   
-  int   first_stripe_base_row = below_numbers_rect.y + 1;  // the "+ 1" represents the tolerance above and below each stripe
-  int   last_stripe_base_row = card_image_size.height - (kSmallCharacterHeight + 1);  // the "+ 1" represents the tolerance above and below each stripe
+  int   first_stripe_base_row = relevant_rect.y + 1;  // the "+ 1" represents the tolerance above and below each stripe
+  int   last_stripe_base_row = card_image_size.height - (minCharacterHeight + 1);  // the "+ 1" represents the tolerance above and below each stripe
   long  line_sum[card_image_size.height];
   
   int   left_edge = kSmallCharacterWidth * 3;  // there aren't usually any actual characters this far to the left
@@ -70,68 +52,77 @@ std::vector<StripeSum> sorted_stripes(IplImage *card_y) {
   
   cvResetImageROI(sobel_image);
   
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
+#if DEBUG_STRIPES_PERFORMANCE
   dmz_debug_timer_print("line sums");
 #endif
   
-  // Determine the 3 most probable, non-overlapping stripes. (Where "stripe" == kSmallCharacterHeight contiguous scan lines.)
-  // (Two will usually get us expiry and name, but some cards have additional distractions.)
-  
-#define kNumberOfStripesToTry 3
   int row;
   std::vector<StripeSum> stripe_sums;
   for (int base_row = first_stripe_base_row; base_row < last_stripe_base_row; base_row++) {
     long sum = 0;
-    for (int row = base_row; row < base_row + kSmallCharacterHeight; row++) {
+    for (int row = base_row; row < base_row + minCharacterHeight; row++) {
       sum += line_sum[row];
     }
     
     // Calculate threshold = half the value of the maximum line-sum in the stripe:
     long threshold = 0;
-    for (row = base_row; row < base_row + kSmallCharacterHeight; row++) {
+    for (row = base_row; row < base_row + minCharacterHeight; row++) {
       if (line_sum[row] > threshold) {
         threshold = line_sum[row];
       }
     }
-    threshold = threshold / 2;
+    threshold /= 2;
     
     // Eliminate stripes that have a a much dimmer-than-average sub-stripe at their very top or very bottom:
     if (line_sum[base_row] + line_sum[base_row + 1] < threshold) {
       continue;
     }
-    if (line_sum[base_row + kSmallCharacterHeight - 2] + line_sum[base_row + kSmallCharacterHeight - 1] < threshold) {
+    if (line_sum[base_row + minCharacterHeight - 2] + line_sum[base_row + kSmallCharacterHeight - 1] < threshold) {
       continue;
     }
     
     // Eliminate stripes that contain a much dimmer-than-average sub-stripe,
     // since that usually means that we've been fooled into grabbing the bottom
     // of some card feature and the top of a different card feature.
-    bool isGoodStrip = true;
-    for (row = base_row; row < base_row + kSmallCharacterHeight - 3; row++) {
+    bool isGoodStripe = true;
+    for (row = base_row; row < base_row + minCharacterHeight - 3; row++) {
       if (line_sum[row + 1] < threshold && line_sum[row + 2] < threshold) {
-        isGoodStrip = false;
+        isGoodStripe = false;
         break;
       }
     }
-    
-    if (isGoodStrip) {
-      StripeSum stripe_sum;
-      stripe_sum.base_row = base_row;
-      stripe_sum.sum = sum;
-      stripe_sums.push_back(stripe_sum);
+    if (!isGoodStripe) {
+      continue;
     }
+
+    StripeSum stripe_sum;
+    stripe_sum.base_row = base_row;
+    stripe_sum.height = minCharacterHeight;
+    stripe_sum.sum = sum;
+
+    // While successive scan line sums are also > threshold, append them to the stripe
+    for (row = base_row + minCharacterHeight; row < last_stripe_base_row && stripe_sum.height <= maxCharacterHeight; row++) {
+      if (line_sum[row] >= threshold) {
+        stripe_sum.height++;
+        stripe_sum.sum += line_sum[row];
+      }
+    }
+
+    // Save the result
+    stripe_sums.push_back(stripe_sum);
   }
   
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
+#if DEBUG_STRIPES_PERFORMANCE
   dmz_debug_timer_print("sum stripes");
 #endif
   
   std::sort(stripe_sums.begin(), stripe_sums.end(), StripeSumCompareDescending());
   
-#if DEBUG_EXPIRY_SEGMENTATION_PERFORMANCE
+#if DEBUG_STRIPES_PERFORMANCE
   dmz_debug_timer_print("sort stripe sums");
 #endif
-  
+
+  return stripe_sums;
 }
 
 #endif // COMPILE_DMZ
